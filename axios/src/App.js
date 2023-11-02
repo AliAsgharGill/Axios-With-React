45ac3feb705840f9744da55a42631433dc3d388f
aliasghargill <stylinalivlogs@gmail.com>
2023-11-02 08:22:00 +0500

    user-list

--- axios/src/App.js
+++ axios/src/App.js
@@ -5,18 +5,29 @@ function App() {
   const [myData, setMyData] = useState([]);
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get("https://jsonplaceholder.typicode.com/users")
       .then((res) => setMyData(res.data));
   }, []);
   return (
     <>
       <h1 className="">Data with Axios</h1>
       {myData.map((post) => {
-        const { id, title, body } = post;
+        const { id, name, username, email, address, company, website, phone } =
+          post;
         return (
-          <div className="" id={id}>
-            <h2>{title}</h2>
-            <h2>{body}</h2>
+          <div
+            key={id}
+            className="  max-w-xs rounded-lg text-center  p-3  shadow-lg border-2  brder-orange-700 bg-orange-500 "
+          >
+            <h2 className="text-lg font-bold">Name: {name}</h2>
+            <h2 className="text-lg font-bold">UserName: {username}</h2>
+            <p className="text-lg ">Email: {email}</p>
+            <p className="text-lg ">City: {address.city}</p>
+            <p className="text-lg ">Zipcode: {address.zipcode}</p>
+            <p className="text-lg ">Email: {email}</p>
+            <p className="text-lg ">Company: {company.name}</p>
+            <p className="text-lg ">Phone: {phone}</p>
+            <p className="text-lg ">Website: {website}</p>
           </div>
         );
       })}
